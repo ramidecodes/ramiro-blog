43641a44c8c1ab16e6d2bfcc8add5eeb46f540b4
Ramiro <ramiro@kryha.io>
2023-11-12 23:13:11 +0100

    Update linkedin URL

--- site.config.ts
+++ site.config.ts
@@ -19,7 +19,7 @@ export default siteConfig({
   // social usernames (optional)
   twitter: 'eco_virtual',
   github: 'ecovirtual',
-  linkedin: 'ramirezramiro/?locale=en_US',
+  linkedin: 'ramirezramiro',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
